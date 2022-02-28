fcbf2d1f8d9e1671747af17b7fbb349e861c137e
301-07\User <l1j1g5@naver.com>
2022-02-28 21:45:37 +0900

    2022-02-28 lesson05 quiz01

---
+++ src/main/java/com/quiz/lesson05/Lesson05Quiz02Controller.java
@@ -0,0 +1,30 @@
+package com.quiz.lesson05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/lesson05")
+@Controller
+public class Lesson05Quiz02Controller {
+	
+	// 요청 url: http://localhost/lesson05/quiz02
+	@RequestMapping("/quiz02")
+	public String quiz02(Model model) {
+		List<String> musicRanking = new ArrayList<>();
+		musicRanking.add("강남스타일");
+		musicRanking.add("벚꽃엔딩");
+		musicRanking.add("좋은날");
+		musicRanking.add("거짓말");
+		musicRanking.add("보고싶다");
+		
+		model.addAttribute("music", musicRanking);
+		
+		return "lesson05/quiz02";
+	}
+}
